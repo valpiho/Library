b908c5923c0c3ce7a1181945dc3b9798340d269f
Alex <boss453@gmail.com>
2020-10-31 21:27:36 +0200

    added @FXML tags with variables for auhor tab

---
+++ src/main/java/org/example/BasicView.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class BasicView {
+
+    //authorTab//
+    @FXML
+    private TextField auBookNameForCreate, auBookDescForCreate, auBookNameForEdit, auBookDescForEdit,
+                        auBookIdForDeleteOrGet, auNameForGetAllBooksAndReviews, auBookIdForEdit;
+
+    @FXML
+    private Button auCreateBook, auEditBook, auDeleteBook, auGetBook, auGetAllBooks, auGetAllReviews;
+
+    @FXML
+    private TextArea auTextAreaForAll;
+    //authorTab//
+    //customerTab//
+   /* @FXML
+    private TextField*/
+}
--- src/main/java/org/example/BasicViews.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import javafx.fxml.FXML;
-
-import java.io.IOException;
-
-public class BasicViews {
-
-}
