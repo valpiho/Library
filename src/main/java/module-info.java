1099220a66eab9a4c37d7aa2d713ac546fb9c682
Alex <boss453@gmail.com>
2020-11-01 12:05:41 +0200

    test branch

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.hibernate.orm.core;
+    requires java.persistence;
 
     opens org.example to javafx.fxml;
     exports org.example;
