99524a9bdf826be6e1c1b472055c9053e2ed0330
Valentin Piho <val.piho@gmail.com>
2020-11-01 09:49:42 +0200

    Panes added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,34 +4,57 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.example.entity.Author;
 import org.example.entity.Book;
 
-import java.io.IOException;
-
 public class App extends Application {
-
-    private final TableView<Book> bookTableView = new TableView<>();
-
     public static void main(String[] args) {
         launch(args);
     }
 
+    Author author1 = new Author("Val", "Piho");
+    Author author2 = new Author("John", "Doe");
+    Author author3 = new Author("Mary", "Jane");
+
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage){
 
-        Scene scene = new Scene(new Group());
-        stage.setWidth(700);
-        stage.setHeight(500);
+        TabPane tabs = new TabPane();
+
+        Tab bookTab = new Tab();
+        bookTab.setText("Books");
+        bookTab.setContent(booksSample());
+
+        Tab authorTab = new Tab();
+        authorTab.setText("Authors");
+        authorTab.setContent(authorSample());
+
+        Tab clientTab = new Tab();
+        authorTab.setText("Clients");
+        authorTab.setContent(null);
+
+        tabs.getTabs().addAll(bookTab, authorTab);
+
+        Scene scene = new Scene(tabs, 650,500);
         stage.setTitle("library");
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    private Pane booksSample() {
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(20, 0, 20, 20));
+
+        TableView<Book> bookTableView = new TableView<>();
         bookTableView.setEditable(true);
 
         // Name Column
@@ -49,27 +72,40 @@ public class App extends Application {
         descriptionColumn.setMinWidth(200);
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
 
-        bookTableView.setItems(getAllBook());
+        bookTableView.setItems(getAllBooks());
         bookTableView.getColumns().addAll(nameColumn, authorColumn, descriptionColumn);
 
-        VBox vBox = new VBox();
-        vBox.setSpacing(5);
-        vBox.setPadding(new Insets(10, 0, 0, 10));
-        vBox.getChildren().addAll(bookTableView);
+        borderPane.setLeft(bookTableView);
 
-        ((Group) scene.getRoot()).getChildren().addAll(vBox);
-
-        stage.setScene(scene);
-        stage.show();
+        return borderPane;
     }
 
-    public ObservableList<Book> getAllBook() {
+    private Pane authorSample() {
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(20, 0, 20, 20));
+
+        TableView<Author> bookTableView = new TableView<>();
+        bookTableView.setEditable(true);
+
+        // Name Column
+        TableColumn<Author, String> firstNameColumn = new TableColumn<>("First name");
+        firstNameColumn.setMinWidth(200);
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+
+        // Author Column
+        TableColumn<Author, String> lastNameColumn = new TableColumn<>("Last name");
+        lastNameColumn.setMinWidth(200);
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+        bookTableView.setItems(getAllAuthors());
+        bookTableView.getColumns().addAll(firstNameColumn, lastNameColumn);
+
+        borderPane.setLeft(bookTableView);
+
+        return borderPane;
+    }
+    public ObservableList<Book> getAllBooks() {
         ObservableList<Book> books = FXCollections.observableArrayList();
-
-        Author author1 = new Author("Val", "Piho");
-        Author author2 = new Author("John", "Doe");
-        Author author3 = new Author("Mary", "Jane");
-
         books.add(new Book("Java from Scratch",
                 author1.getFullName(),
                 "Some interesting book of Java"));
@@ -80,5 +116,13 @@ public class App extends Application {
                 author3.getFullName(),
                 "Some interesting book of JavaFX"));
         return books;
+    }
+
+    public ObservableList<Author> getAllAuthors() {
+        ObservableList<Author> authors = FXCollections.observableArrayList();
+        authors.add(author1);
+        authors.add(author2);
+        authors.add(author3);
+        return authors;
     }
 }
